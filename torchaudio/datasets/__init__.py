25d58bf2074d3ca0a5f1b56db0907d36875d7100
jnistal <j.nistalhurle@gmail.com>
2019-04-03 17:30:21 +0200

    Added new sinewave loader

--- torchaudio/datasets/__init__.py
+++ torchaudio/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .yesno import YESNO
 from .vctk import VCTK
 from .youtube_pianos import YOUTUBE_PIANOS
+from .sinewave_loader import SinewaveLoader
 
-__all__ = ('YESNO', 'VCTK', 'YOUTUBE_PIANOS')
+__all__ = ('YESNO', 'VCTK', 'YOUTUBE_PIANOS', 'SinewaveLoader')
